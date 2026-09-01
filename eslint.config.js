e23b825e03fdb4fd59464e0708d75681ab98a847
Takuto Nagami <logica0419@gmail.com>
2026-09-02 08:09:54 +0900

    add auto-fix of no-unused-vars to eslint
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,24 @@
+import tsLint from "@typescript-eslint/eslint-plugin";
 import eslintPluginAstro from "eslint-plugin-astro";
 
-export default eslintPluginAstro.configs.recommended;
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["*.astro", "**/*.astro"],
+    plugins: {
+      "@typescript-eslint": tsLint,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrors: "none",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_",
+          enableAutofixRemoval: { imports: true },
+        },
+      ],
+    },
+  },
+];
