5ad520bd3ef7fec9bfe4524b4d170b6c5fec3bed
Takuto Nagami <logica0419@gmail.com>
2026-09-02 00:52:58 +0900

    add knip
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,9 @@ import type { StorybookConfig } from "@storybook-astro/framework";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  core: {
+    builder: "@storybook/builder-vite",
+  },
   framework: {
     name: "@storybook-astro/framework",
     options: {},
