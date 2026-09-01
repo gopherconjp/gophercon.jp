c775d0e5a7604237f4d090f479086a93ec5d0691
Takuto Nagami <logica0419@gmail.com>
2026-09-02 01:11:28 +0900

    fix eslint settings
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,3 @@
-import tsParser from "@typescript-eslint/parser";
-import * as astroParser from "astro-eslint-parser";
 import eslintPluginAstro from "eslint-plugin-astro";
 
-export default [
-  ...eslintPluginAstro.configs["flat/recommended"],
-  {
-    files: ["**/*.astro"],
-    languageOptions: {
-      parser: astroParser,
-      parserOptions: {
-        parser: tsParser,
-        extraFileExtensions: [".astro"],
-      },
-    },
-  },
-];
+export default eslintPluginAstro.configs.recommended;
