11f22251faac8ed5d218c203dcc289b16c5bbf7e
Takuto Nagami <logica0419@gmail.com>
2026-09-10 23:53:22 +0900

    Remove initial args from CardList stories and reset Default export
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- src/components/2027/_common/card/List.stories.tsx
+++ src/components/2027/_common/card/List.stories.tsx
@@ -4,15 +4,11 @@ export default {
   title: "2027/Common/Card/List",
   component: CardList,
   args: {
-    cards: [],
-  },
-};
-
-export const Default = {
-  args: {
     cards: [
       { label: "Date", content: "Feb 13-14" },
       { label: "Venue", content: "Abema Towers", body: "Shibuya, Tokyo" },
     ],
   },
 };
+
+export const Default = {};
