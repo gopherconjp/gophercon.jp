ae5890ccc995a85c5e7e28a54b0804e73cf51eb7
Takuto Nagami <logica0419@gmail.com>
2026-09-16 21:03:50 +0900

    Add Markdown common component for 2027
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

---
+++ src/content.config.ts
@@ -0,0 +1,8 @@
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+
+const markdown2027 = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/2027" }),
+});
+
+export const collections = { markdown2027 };
