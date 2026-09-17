ce234986dc05e46567484c93a5202a28464d123e
Takuto Nagami <logica0419@gmail.com>
2026-09-18 03:36:27 +0900

    Support slot content and external links in NavLink with MDI icons
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- src/components/2027/_common/nav-link/index.stories.tsx
+++ src/components/2027/_common/nav-link/index.stories.tsx
@@ -4,14 +4,16 @@ export default {
   title: "2027/Common/NavLink",
   component: NavLink,
   args: {
-    label: "About",
     href: "#about",
     current: false,
   },
 };
 
-export const Default = {};
+export const Default = {
+  render: (args: Record<string, unknown>) => <NavLink {...args}>About</NavLink>,
+};
 
 export const Current = {
   args: { current: true },
+  render: (args: Record<string, unknown>) => <NavLink {...args}>About</NavLink>,
 };
