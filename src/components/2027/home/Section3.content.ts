89ba7af69206508a44ba9aae13ceb62a507c8d07
Takuto Nagami <logica0419@gmail.com>
2026-09-18 20:49:33 +0900

    Adapt home Section3 to Breakable space prop and nested copy
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- src/components/2027/home/Section3.content.ts
+++ src/components/2027/home/Section3.content.ts
@@ -8,20 +8,23 @@ const content = {
   key,
   content: {
     title: t<Breakable>({
-      en: "We want your proposals!",
-      ja: "プロポーザル大募集！",
+      en: ["We want", ["your", "proposals!"]],
+      ja: ["プロポーザル", "大募集！"],
     }),
     body: t<Breakable>({
       en: [
         "We're calling for proposals!",
         [
-          "Want to be part GopherCon Japan? Check out our",
-          { link: "CFP Page →", href: "/2027/cfp" },
+          ["Want to be part", "GopherCon Japan?"],
+          ["Check out our", { link: "CFP Page →", href: "/2027/cfp" }],
         ],
       ],
       ja: [
-        "GopherCon Japanでは、カンファレンスを盛り上げてくれるプロポーザルを募集しています。",
-        [["詳しくは", { link: "CFPページ", href: "/ja/2027/cfp" }, "を"], "ご覧ください。"],
+        [
+          ["GopherCon Japanでは、", "カンファレンスを"],
+          ["盛り上げてくれる", "プロポーザルを募集しています。"],
+        ],
+        [["詳しくは", [{ link: "CFPページ", href: "/ja/2027/cfp" }, "を"]], "ご覧ください。"],
       ],
     }),
   },
