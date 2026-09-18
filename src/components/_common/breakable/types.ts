a4df4f66fd629294d343ae6a88cd218fd81be9c4
Takuto Nagami <logica0419@gmail.com>
2026-09-18 20:45:53 +0900

    refactor: add space prop and recursive break detection to Breakable

--- src/components/_common/breakable/types.ts
+++ src/components/_common/breakable/types.ts
@@ -7,9 +7,13 @@ interface Link {
   link: Breakable;
 }
 
-export type Breakable = Breakable[] | Accent | Link | string;
+interface Break {
+  break: true;
+}
 
-const isObject = (value: Breakable): value is Accent | Link =>
+export type Breakable = Breakable[] | Accent | Link | Break | string;
+
+const isObject = (value: Breakable): value is Accent | Link | Break =>
   typeof value === "object" && value !== null && !Array.isArray(value);
 
 export const isAccent = (value: Breakable): value is Accent =>
@@ -17,3 +21,6 @@ export const isAccent = (value: Breakable): value is Accent =>
 
 export const isLink = (value: Breakable): value is Link =>
   isObject(value) && Object.hasOwn(value, "link");
+
+export const isBreak = (value: Breakable): value is Break =>
+  isObject(value) && "break" in value && value.break;
